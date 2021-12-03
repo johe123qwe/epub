d17fa96156646258b8f3ecea57ae8f0449d9790b
xy <renshenglu1010@gmail.com>
2021-12-03 20:51:30 +0100

    update tinfy_png.py version

--- tinify_png.py
+++ tinify_png.py
@@ -1,60 +1,99 @@
-import tinify
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+pip install tinify
+'''
+
 import os
-from datetime import datetime
+import sys
+import os.path
+import click
+import tinify
 # @ 使用熊猫压缩图片 https://tinypng.com/
-# modified by xy 2020-12-08 修改输出文件名
-# modified by xy 2020-12-31 改为从文件夹里转换多个图片
 
-'''
-pip install --upgrade tinify
-'''
+tinify.key = "YOURKEY" # API KEY
+version = "1.4"   # 版本
+# modified by xy 2021-11-23 python2 to python3
+# modified by xy 2021-12-03 输出文件大小
 
-version_ = '1.3'
-key = "your key"
-def check_api(api_key):
-    '''校验api有效性'''
-    try:
-      tinify.key = api_key
-      tinify.validate()
-    except tinify.Error as e:
-      # Validation of API key failed.
-      pass
 
-def comress_img(source_file):
-    '''压缩图片'''
-    try:
-      # Use the Tinify API client.
-        source = tinify.from_file(source_file)
-        now_time = str(datetime.now().strftime('%H%M%S'))
-        output_file = os.path.join(os.path.dirname(source_file), \
-            (now_time + '_' + os.path.basename(source_file)))
-        source.to_file(output_file)
-        print('已压缩', output_file)
-        print('本月已使用', tinify.compression_count, '次,', '共500次。')
-    except tinify.AccountError as e:
-      print("The error message is: %s" % (e.message))
-      # Verify your API key and account limit.
-    except tinify.ClientError as e:
-      # Check your source image and request options.
-      pass
-    except tinify.ServerError as e:
-      # Temporary issue with the Tinify API.
-      pass
-    except tinify.ConnectionError as e:
-      # A network connection error occurred.
-      pass
-    except Exception as e:
-      # Something else went wrong, unrelated to the Tinify API.
-      pass
+# 获取文件大小
+def get_file_size(file_path):
+    """将文件大小转为 MB，保留小数点后两位"""
+    size = format(os.path.getsize(file_path)/(1024*1024), '.2f')
+    print(os.path.basename(file_path), size, 'MB')
+
+
+# 压缩的核心
+def compress_core(inputFile, outputFile, img_width):
+    source = tinify.from_file(inputFile)
+    if img_width != -1:
+        resized = source.resize(method = "scale", width  = img_width)
+        resized.to_file(outputFile)
+    else:
+        source.to_file(outputFile)
+        get_file_size(outputFile)
+
+# 压缩一个文件夹下的图片
+def compress_path(path, width):
+    print("compress_path-------------------------------------")
+    if not os.path.isdir(path):
+        print("这不是一个文件夹，请输入文件夹的正确路径!")
+        return
+    else:
+        fromFilePath = path     # 源路径
+        toFilePath = path+"/tiny"   # 输出路径
+        print("fromFilePath=%s" %fromFilePath)
+        print("toFilePath=%s" %toFilePath)
+
+        for root, dirs, files in os.walk(fromFilePath):
+            print("root = %s" %root)
+            print("dirs = %s" %dirs)
+            print("files= %s" %files)
+            for name in files:
+                fileName, fileSuffix = os.path.splitext(name)
+                if fileSuffix == '.png' or fileSuffix == '.jpg' or fileSuffix == '.jpeg':
+                    toFullPath = toFilePath + root[len(fromFilePath):]
+                    toFullName = toFullPath + '/' + name
+                    if os.path.isdir(toFullPath):
+                        pass
+                    else:
+                        os.mkdir(toFullPath)
+                    compress_core(root + '/' + name, toFullName, width)
+            break   # 仅遍历当前目录
+
+# 仅压缩指定文件
+def compress_file(inputFile, width):
+    print("compress_file-------------------------------------")
+    if not os.path.isfile(inputFile):
+        print("这不是一个文件，请输入文件的正确路径!")
+        return
+    print("file = %s" %inputFile)
+    dirname  = os.path.dirname(inputFile)
+    basename = os.path.basename(inputFile)
+    fileName, fileSuffix = os.path.splitext(basename)
+    if fileSuffix == '.png' or fileSuffix == '.jpg' or fileSuffix == '.jpeg':
+        compress_core(inputFile, dirname+"/tiny_"+basename, width)
+    else:
+        print("不支持该文件类型!")
+
+@click.command()
+@click.option('-f', "--file",  type=str,  default=None,  help="单个文件压缩")
+@click.option('-d', "--dir",   type=str,  default=None,  help="被压缩的文件夹")
+@click.option('-w', "--width", type=int,  default=-1,    help="图片宽度，默认不变")
+def run(file, dir, width):
+    print ("GcsSloop TinyPng V%s" %(version))
+    if file is not None:
+        compress_file(file, width)  # 仅压缩一个文件
+        pass
+    elif dir is not None:
+        compress_path(dir, width)   # 压缩指定目录下的文件
+        pass
+    else:
+        compress_path(os.getcwd(), width)   # 压缩当前目录下的文件
+    print('本月已使用：', tinify.compression_count, '/500 次。', sep='')
+    print("结束!")
 
 if __name__ == "__main__":
-    check_api(key)
-    antwort = input('是否保留原始图片(yes/no): ')
-    for root, dirs, files in os.walk('/home/mount_mac/test/35'):
-        files = [f for f in files if not f[0] == '.']
-        dirs[:] = [d for d in dirs if not d[0] == '.']
-        for f_path in files:
-            r_path = os.path.join(root, f_path)
-            comress_img(r_path)
-            if antwort == 'no' or antwort == 'NO' or antwort == 'n' or antwort == 'N':
-                os.remove(r_path)
+    run()
